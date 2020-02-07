b979499b4b3921e01f47928945ad6035658ced5d
daachi08 <60571463+daachi08@users.noreply.github.com>
2020-02-07 19:52:53 +0900

    Create content-archive.php

---
+++ content-archive.php
@@ -0,0 +1,10 @@
+<a href="<?php the_permalink(); ?>" class="archive-link">
+                            <div class="news-body">
+                                <time class="release"><?php the_time( 'Y.m.d'); ?>
+                                </time>
+                                    <p class="archive-title">
+                                        <?php the_title(); ?>
+                                    </p>
+
+                            </div>
+                        </a>
