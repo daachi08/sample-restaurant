df3b7530531fe57634bd0b5cbf0313b316af28b3
daachi08 <60571463+daachi08@users.noreply.github.com>
2020-02-07 19:52:18 +0900

    Create content-access.php

---
+++ content-access.php
@@ -0,0 +1,23 @@
+<section class="access">
+            <div class="access-wrapper">
+                <iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.8278533985103!2d139.76493611492506!3d35.68124053757957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bfbd89f700b%3A0x277c49ba34ed38!2z5p2x5Lqs6aeF!5e0!3m2!1sja!2sjp!4v1576669932265!5m2!1sja!2sjp" width="100%" height="600" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                <div class="access-boxs">
+                    <div class="access-heading_pc">
+                        <div class="access-icon"><i class="fas fa-times"></i></div>
+                            <h2 class="access-title">ACCESS</h2>
+                            <p class="border-line_access"></p>
+                            <p class="access-text">東京駅より徒歩5分・・・・・・</p>
+                    </div><!--access-heading_pc-->
+                </div><!--access-boxs-->
+                    <div class="access-wrapepr_sp">
+                        <div class="inner">
+                            <div class="access-heading_sp">
+                            <h2 class="title">ACCESS</h2>
+                            <p class="border-line"></p>
+                            <p class="sub-title">東京駅より徒歩5分</p>  
+                            </div><!--access-heading_sp-->
+                        </div><!--inner-->
+                    </div><!--access-wrapper_sp-->
+            </div><!--access-wrapper-->
+
+        </section><!--access-->
